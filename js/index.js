d646b6a4af93d173e47841d4b7ef380268eb653e
mateusnagelo <mateusgenius1234@hotmail.com>
2022-10-03 21:35:10 -0300

    First commit

---
+++ js/index.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const limparFormulario = (endereco) => {
+    document.getElementById('endereco').value = '';
+    document.getElementById('bairro').value = '';
+    document.getElementById('cidade').value = '';
+    document.getElementById('estado').value = '';
+}
+
+
+const preencherFormulario = (endereco) => {
+    document.getElementById('endereco').value = endereco.logradouro;
+    document.getElementById('bairro').value = endereco.bairro;
+    document.getElementById('cidade').value = endereco.localidade;
+    document.getElementById('estado').value = endereco.uf;
+}
+
+
+const eNumero = (numero) => /^[0-9]+$/.test(numero);
+
+const cepValido = (cep) => cep.length == 8 && eNumero(cep);
+
+const pesquisarCep = async() => {
+    limparFormulario();
+
+    const cep = document.getElementById('cep').value.replace("-", "");
+    const url = `https://viacep.com.br/ws/${cep}/json/`;
+    if (cepValido(cep)) {
+        const dados = await fetch(url);
+        const endereco = await dados.json();
+        if (endereco.hasOwnProperty('erro')) {
+            document.getElementById('endereco').value = 'CEP não encontrado!';
+        } else {
+            preencherFormulario(endereco);
+        }
+    } else {
+        document.getElementById('endereco').value = 'CEP incorreto!';
+    }
+
+}
+
+document.getElementById('cep')
+    .addEventListener('focusout', pesquisarCep);
+
+
+$("#cep").mask("00000-000", { reverse: true })
+
+$("#numero").mask("000000")
+
+$("#celular").mask("(00) 00000-0000", {
+
+})
+
+var options = {
+    translation: {
+        'A': { pattern: /[A-Z]/ },
+        'a': { pattern: /[a-zA-Z]/ },
+        'S': { pattern: /[a-zA-Z0-9]/ },
+        'L': { pattern: /[a-z]/ },
+    }
+}
+
+function newFunction() {
+    return $(document).ready
+}
